ad5e0ad2df0637ab99a7643ac540081f6618ce1c
Riley Sumner <rsumner@wisc.edu>
2020-05-26 12:18:22 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterSelectComponent } from './character-select/character-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterSelectComponent
   ],
   imports: [
     BrowserModule,
