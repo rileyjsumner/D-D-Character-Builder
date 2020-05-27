ea7eb35ecc8d9743503224dcf943ddba5b61c525
Riley Sumner <rsumner@wisc.edu>
2020-05-26 20:43:54 -0500

    create character

--- src/app/character-select/character-select.component.ts
+++ src/app/character-select/character-select.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-character-select',
   templateUrl: './character-select.component.html',
-  styleUrls: ['./character-select.component.scss']
+  styleUrls: ['./character-select.component.scss'],
 })
 export class CharacterSelectComponent implements OnInit {
+  private character: Character = {
+    id: 1,
+    name: 'Glynbalar'
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
