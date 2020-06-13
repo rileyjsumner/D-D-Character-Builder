35d271b9e3322ebfde9356b3986e465bacfbb7df
Riley Sumner <rsumner@wisc.edu>
2020-06-13 09:07:05 -0500

    bonds/flaws/traits

--- src/app/character-modules/character-add/character-add.component.ts
+++ src/app/character-modules/character-add/character-add.component.ts
@@ -1209,7 +1209,7 @@ export class CharacterAddComponent implements OnInit {
             'Noble Obligation. It is my duty to protect and care for the people beneath me. (Good)'];
         this.bondRolls = ['',
             'I will face any challenge to win the approval of my family.',
-            'My house’s alliance with another noble family must be sustained at all costs.',
+            'My house\'s alliance with another noble family must be sustained at all costs.',
             'Nothing is more important than the other members of my family.',
             'I am in love with the heir of a family that my family despises.',
             'My loyalty to my sovereign is unwavering.',
@@ -1217,7 +1217,7 @@ export class CharacterAddComponent implements OnInit {
         this.flawRolls = ['',
             'I secretly believe that everyone is beneath me.',
             'I hide a truly scandalous secret that could ruin my family forever.',
-            'I too often hear veiled insults and threats in every word addressed to me, and I’m quick to anger.',
+            'I too often hear veiled insults and threats in every word addressed to me, and I\'m quick to anger.',
             'I have an insatiable desire for carnal pleasures.',
             'In fact, the world does revolve around me.',
             'By my words and actions, I often bring shame to my family.'];
@@ -1226,24 +1226,174 @@ export class CharacterAddComponent implements OnInit {
       case 'Outlander': {
         this.abilityDetails.patchValue({AthleticsProf: true, SurvivalProf: true});
         // Origin
+        this.traitRolls = ['',
+            'I\'m driven by a wanderlust that led me away from home.',
+            'I watch over my friends as if they were a litter of newborn pups.',
+            'I once ran twenty-five miles without stopping to warn to my clan of an approaching orc horde. I\'d do it again if I had to.',
+            'I have a lesson for every situation, drawn from observing nature.',
+            'I place no stock in wealthy or well-mannered folk. Money and manners won\'t save you from a hungry owlbear.',
+            'I\'m always picking things up, absently fiddling with them, and sometimes accidentally breaking them.',
+            'I feel far more comfortable around animals than people.',
+            'I was, in fact, raised by wolves.'];
+        this.idealRolls = ['',
+            'Change. Life is like the seasons, in constant change, and we must change with it. (Chaotic)',
+            'Greater Good. It is each person\'s responsibility to make the most happiness for the whole tribe. (Good)',
+            'Honor. If I dishonor myself, I dishonor my whole clan. (Lawful)',
+            'Might. The strongest are meant to rule. (Evil)',
+            'Nature. The natural world is more important than all the constructs of civilization. (Neutral)',
+            'Glory. I must earn glory in battle, for myself and my clan. (Any)'];
+        this.bondRolls = ['',
+            'My family, clan, or tribe is the most important thing in my life, even when they are far from me.',
+            'An injury to the unspoiled wilderness of my home is an injury to me.',
+            'I will bring terrible wrath down on the evildoers who destroyed my homeland.',
+            'I am the last of my tribe, and it is up to me to ensure their names enter legend.',
+            'I suffer awful visions of a coming disaster and will do anything to prevent it.',
+            'It is my duty to provide children to sustain my tribe.'];
+        this.flawRolls = ['',
+            'I am too enamored of ale, wine, and other intoxicants.',
+            'There\'s no room for caution in a life lived to the fullest.',
+            'I remember every insult I\'ve received and nurse a silent resentment toward anyone who\'s ever wronged me.',
+            'I am slow to trust members of other races, tribes, and societies.',
+            'Violence is my answer to almost any challenge.',
+            'Don\'t expect me to save those who can\'t save themselves. It is nature\'s way that the strong thrive and the weak perish.'];
         break;
       }
       case 'Sage': {
         this.abilityDetails.patchValue({ArcanaProf: true, HistoryProf: true});
         // Specialty
+        this.traitRolls = ['',
+            'I use polysyllabic words that convey the impression of great erudition.',
+            'I\'ve read every book in the world\'s greatest libraries — or I like to boast that I have.',
+            'I\'m used to helping out those who aren\'t as smart as I am, and I patiently explain anything and everything to others.',
+            'There\'s nothing I like more than a good mystery.',
+            'I\'m willing to listen to every side of an argument before I make my own judgment.',
+            'I ... speak ... slowly ... when talking ... to idiots, ... which ... almost ... everyone ... is ... compared ... to me.',
+            'I am horribly, horribly awkward in social situations.',
+            'I\'m convinced that people are always trying to steal my secrets.'];
+        this.idealRolls = ['',
+            'Knowledge. The path to power and self-improvement is through knowledge. (Neutral)',
+            'Beauty. What is beautiful points us beyond itself toward what is true. (Good)',
+            'Logic. Emotions must not cloud our logical thinking. (Lawful)',
+            'No Limits. Nothing should fetter the infinite possibility inherent in all existence. (Chaotic)',
+            'Power. Knowledge is the path to power and domination. (Evil)',
+            'Self-Improvement. The goal of a life of study is the betterment of oneself. (Any)'];
+        this.bondRolls = ['',
+            'It is my duty to protect my students.',
+            'I have an ancient text that holds terrible secrets that must not fall into the wrong hands.',
+            'I work to preserve a library, university, scriptorium, or monastery.',
+            'My life\'s work is a series of tomes related to a specific field of lore.',
+            'I\'ve been searching my whole life for the answer to a certain question.',
+            'I sold my soul for knowledge. I hope to do great deeds and win it back.'];
+        this.flawRolls = ['',
+            'I am easily distracted by the promise of information.',
+            'Most people scream and run when they see a demon. I stop and take notes on its anatomy.',
+            'Unlocking an ancient mystery is worth the price of a civilization.',
+            'I overlook obvious solutions in favor of complicated ones.',
+            'I speak without really thinking through my words, invariably insulting others.',
+            'I can\'t keep a secret to save my life, or anyone else\'s.'];
         break;
       }
       case 'Sailor': {
         this.abilityDetails.patchValue({AthleticsProf: true, PerceptionProf: true});
+        this.traitRolls = ['',
+            'My friends know they can rely on me, no matter what.',
+            'I work hard so that I can play hard when the work is done.',
+            'I enjoy sailing into new ports and making new friends over a flagon of ale.',
+            'I stretch the truth for the sake of a good story.',
+            'To me, a tavern brawl is a nice way to get to know a new city.',
+            'I never pass up a friendly wager.',
+            'My language is as foul as an otyugh nest.',
+            'I like a job well done, especially if I can convince someone else to do it.'];
+        this.idealRolls = ['',
+            'Respect. The thing that keeps a ship together is mutual respect between captain and crew. (Good)',
+            'Fairness. We all do the work, so we all share in the rewards. (Lawful)',
+            'Freedom. The sea is freedom— the freedom to go anywhere and do anything. (Chaotic)',
+            'Mastery. I\'m a predator, and the other ships on the sea are my prey. (Evil)',
+            'People. I\'m committed to my crewmates, not to ideals. (Neutral)',
+            'Aspiration. Someday I\'ll own my own ship and chart my own destiny. (Any)'];
+        this.bondRolls = ['',
+            'I\'m loyal to my captain first, everything else second.',
+            'The ship is most important— crewmates and captains come and go.',
+            'I\'ll always remember my first ship.',
+            'In a harbor town, I have a paramour whose eyes nearly stole me from the sea.',
+            'I was cheated out of my fair share of the profits, and I want to get my due.',
+            'Ruthless pirates murdered my captain and crewmates, plundered our ship, and left me to die. Vengeance will be mine.'];
+        this.flawRolls = ['',
+            'I follow orders, even if I think they\'re wrong.',
+            'I\'ll say anything to avoid having to do extra work.',
+            'Once someone questions my courage, I never back down no matter how dangerous the situation.',
+            'Once I start drinking, it\'s hard for me to stop.',
+            'I can\'t help but pocket loose coins and other trinkets I come across.',
+            'My pride will probably lead to my destruction.'];
         break;
       }
       case 'Soldier': {
         this.abilityDetails.patchValue({AthleticsProf: true, IntimidationProf: true});
         // Specialty
+        this.traitRolls = ['',
+            'I\'m always polite and respectful.',
+            'I\'m haunted by memories o f war. I can\'t get the images of violence out of my mind.',
+            'I\'ve lost too many friends, and I\'m slow to make new ones.',
+            'I\'m full of inspiring and cautionary tales from my military experience relevant to almost every combat situation.',
+            'I can stare down a hell hound without flinching.',
+            'I enjoy being strong and like breaking things.',
+            'I have a crude sense of humor.',
+            'I face problems head-on. A simple, direct solution is the best path to success.'];
+        this.idealRolls = ['',
+            'Greater Good. Our lot is to lay down our lives in defense of others. (Good)',
+            'Responsibility. I do what I must and obey just authority. (Lawful)',
+            'Independence. When people follow orders blindly, they embrace a kind of tyranny. (Chaotic)',
+            'Might. In life as in war, the stronger force wins. (Evil)',
+            'Live and Let Live. Ideals aren\'t worth killing over or going to war for. (Neutral)',
+            'Nation. My city, nation, or people are all that matter. (Any)'];
+        this.bondRolls = ['',
+            'I would still lay down my life for the people I served with.',
+            'Someone saved my life on the battlefield. To this day, I will never leave a friend behind.',
+            'My honor is my life.',
+            'I\'ll never forget the crushing defeat my company suffered or the enemies who dealt it.',
+            'Those who fight beside me are those worth dying for.',
+            'I fight for those who cannot fight for themselves.'];
+        this.flawRolls = ['',
+            'The monstrous enemy we faced in battle still leaves me quivering with fear.',
+            'I have little respect for anyone who is not a proven warrior.',
+            'I made a terrible mistake in battle cost many lives — and I would do anything to keep that mistake secret.',
+            'My hatred of my enemies is blind and unreasoning.',
+            'I obey the law, even if the law causes misery.',
+            'I\'d rather eat my armor than admit when I\'m wrong.'];
         break;
       }
       case 'Urchin': {
         this.abilityDetails.patchValue({SleightOfHandProf: true, StealthProf: true});
+        this.traitRolls = ['',
+            'I hide scraps of food and trinkets away in my pockets.',
+            'I ask a lot of questions.',
+            'I like to squeeze into small places where no one else can get to me.',
+            'I sleep with my back to a wall or tree, with everything I own wrapped in a bundle in my arms.',
+            'I eat like a pig and have bad manners.',
+            'I think anyone who\'s nice to me is hiding evil intent.',
+            'I don\'t like to bathe.',
+            'I bluntly say what other people are hinting at or hiding.'];
+        this.idealRolls = ['',
+            'Respect. All people, rich or poor, deserve respect. (Good)',
+            'Community. We have to take care of each other, because no one else is going to do it. (Lawful)',
+            'Change. The low are lifted up, and the high and mighty are brought down. Change is the nature of things. (Chaotic)',
+            'Retribution. The rich need to be shown what life and death are like in the gutters. (Evil)',
+            'People. I help the people who help me — that\'s what keeps us alive. (Neutral)',
+            'Aspiration. I\'m going to prove that I\'m worthy of a better life.'];
+        this.bondRolls = ['',
+            'My town or city is my home, and I\'ll fight to defend it.',
+            'I sponsor an orphanage to keep others from enduring what I was forced to endure.',
+            'I owe my survival to another urchin who taught me to live on the streets.',
+            'I owe a debt I can never repay to the person who took pity on me.',
+            'I escaped my life of poverty by robbing an important person, and I\'m wanted for it.',
+            'No one else should have to endure the hardships I\'ve been through.'];
+        this.flawRolls = ['',
+            'If I\'m outnumbered, I will run away from a fight.',
+            'Gold seems like a lot of money to me, and I\'ll do just about anything for more of it',
+            'I will never fully trust anyone other than myself.',
+            'I\'d rather kill someone in their sleep then fight fair.',
+            'It\'s not stealing if I need it more than someone else.',
+            'People who can\'t take care of themselves get what they deserve.']
         break;
       }
     }
@@ -1273,9 +1423,19 @@ export class CharacterAddComponent implements OnInit {
     switch(trait) {
       case 'ideals': {
         this.characterTraits.patchValue({ideals: this.idealRolls[roll]});
+        break;
       }
       case 'bonds': {
         this.characterTraits.patchValue({bonds: this.bondRolls[roll]});
+        break;
+      }
+      case 'flaws': {
+        this.characterTraits.patchValue({flaws: this.flawRolls[roll]});
+        break;
+      }
+      case 'personalityTraits': {
+        this.characterTraits.patchValue({personalityTraits: this.traitRolls[roll]});
+        break;
       }
     }
   }
